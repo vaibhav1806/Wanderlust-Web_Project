fe16f5b83d3a9e61639b81afb00a92d5a28ffc35
vaibhav1806 <vaibhavbalip1806@gmail.com>
2024-08-29 21:10:42 +0530

    Your descriptive commit message

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ if (process.env.NODE_ENV != "production") {
     require('dotenv').config();
 }
 
-
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
@@ -20,7 +19,6 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const localStrategy = require("passport-local");
 const User = require("./models/user.js");
-
 const listingRouter = require("./routes/listings.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
@@ -29,53 +27,49 @@ const dbUrl = process.env.ATLASDB_URL;
 
 
 main()
-.then((res)=>{
-    console.log("Connnected to db");
-})
-.catch((err)=>{
-    console.log(err);
-})
+    .then((res) => {
+        console.log("Connnected to db");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
-async function main(){
+async function main() {
     await mongoose.connect(dbUrl);
 }
 
 // middle Ware
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsmate);
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
-    crypto: {   
-        secret:process.env.SECRET
+    crypto: {
+        secret: process.env.SECRET
     },
-    touchAfter: 24*3600,
+    touchAfter: 24 * 3600,
 })
-store.on("error", ()=>{
+store.on("error", () => {
     console.log("Error in mongoStore", error);
-     
+
 })
 const sessionOption = {
     store,
-    secret : process.env.SECRET,
-    resave : false,
-    saveUninitialized : true,
-    cookie : {
-        expires:Date.now() + 7*24*60*60*1000,
-        maxAge:7*24*60*60*1000,
-        http:true
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        http: true
     }
 }
-
-
-
-
 
 // middleWare
 
@@ -84,42 +78,38 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new localStrategy(User.authenticate()));
-
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // Flash message
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     res.locals.currUser = req.user;
     next();
 });
 
-app.use("/listings",listingRouter);
-app.use("/listings/:id/reviews",reviewRouter);
-app.use("/",userRouter);
-
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 
 // for all
-app.all("*",(req,res,next)=>{
+app.all("*", (req, res, next) => {
 
-    next(new ExpressError(555,"Page not found !"));
-    
+    next(new ExpressError(555, "Page not found !"));
+
 });
 
 //Costom err
-app.use((err,req,res,next)=>{
-    let{statusCode =500 , message ="Something Went Wrong"} = err;
-    
-    res.render("listings/error.ejs",{message})
-   
-}); 
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Something Went Wrong" } = err;
 
+    res.render("listings/error.ejs", { message })
 
+});
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
 
     console.log("server is listining at 8080");
 
